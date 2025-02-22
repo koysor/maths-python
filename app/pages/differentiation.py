e4b635130b1dd9558ed62c3520a9df05582f2dda
Koysor <koysor@gmail.com>
2025-02-22 22:38:15 +0000

    Added examples of the chain rule

--- app/pages/differentiation.py
+++ app/pages/differentiation.py
@@ -38,7 +38,7 @@ expression = "x^2 + 3x + 5"
 latex_code = f"""
 f(x) = {expression}
 """
-st.latex(latex_code,)
+st.latex(latex_code)
 
 code_snippet = """
 import sympy as sp
@@ -59,6 +59,8 @@ st.write(f"Second Derivative: {f_double_prime}")
 
 st.code(code_snippet)
 exec(code_snippet)
+
+st.write("Note that sp.symbols() tells Sympy to treat x as a mathematical variale or symbol and not as a standard Python variable.")
 
 
 st.markdown("#### Differentiation Example Using the Finite Differences Method")
@@ -123,7 +125,55 @@ st.markdown("#### The Chain Rule")
 col1, col2 = st.columns([4,3])
 
 with col1:
-    pass
+    st.write("The chain rule is used to differentiate composite functions.  It allows you to differentiate a function nested within another function.")
+    latex_code = r"""
+    y = f(g(x))
+    """
+    st.latex(latex_code)
+
+    st.write("Example:")
+    st.write("Substitute the inside function with u.")
+    latex_code = r"""
+    y = \sqrt{x^3 + 1} \\
+    u = x^3 + 1 \\
+    y = \sqrt{u} = u^{1/2}
+    """
+    st.latex(latex_code)
+
+    st.write("Treat u as a single variable and differentiate.")
+    latex_code = r"""
+    \frac{dy}{du} = \frac{1}{2}u^{-1/2}
+    """
+    st.latex(latex_code)
+
+    st.write("Replace u with the inside function.")
+    latex_code = r"""
+    \frac{dy}{du} = \frac{1}{2}(x^3 + 1)^{-1/2} \\
+    = \frac{1}{2\times\sqrt{x^3 + 1}}
+    """
+    st.latex(latex_code)
+
+    st.write("Multiply the result by the derivative of u.")
+    latex_code = r"""
+    \frac{du}{dx} = 3x^2 \\
+    \frac{dy}{dx} = \frac{dy}{du} \times \frac{du}{dx} \\
+    = \frac{3x^2}{2\times\sqrt{x^3 + 1}}
+    """
+    st.latex(latex_code)
+
+    code_snippet = """
+from sympy import symbols, diff
+
+x = symbols("x")
+y = (x**3 + 1)**(1/2)
+
+# Compute the derivative using chain rule
+dy_dx = diff(y, x)
+
+st.write(dy_dx)
+    """
+    st.code(code_snippet)
+    exec(code_snippet)
 
 with col2:
     st.video("https://www.youtube.com/watch?v=9IAUw5brhf4&list=PLHnDkwDE03A88Tj3mrg_LRpUWqyVipHkM&index=87&pp=iAQB")
@@ -193,3 +243,14 @@ with col1:
 
 with col2:
     st.video("https://www.youtube.com/watch?v=FPFrDxl8GxQ&list=PLHnDkwDE03A88Tj3mrg_LRpUWqyVipHkM&index=98&pp=iAQB")
+
+
+st.markdown("#### Solving Differential Equations")
+
+col1, col2 = st.columns([4,3])
+
+with col1:
+    pass
+
+with col2:
+    st.video("https://www.youtube.com/watch?v=ao9YmyNTLN4&list=PLHnDkwDE03A88Tj3mrg_LRpUWqyVipHkM&index=112")
--- app/pages/integration.py
+++ app/pages/integration.py
@@ -4,11 +4,18 @@ import streamlit as st
 st.set_page_config(layout="wide")
 st.markdown("### Integration")
 
-summary_integration = """
-Integration is the opposite of differentiation.
-\nFor a given function f(x), integration is used to find the area under the curve of the function between two points on the x-axis.
-"""
-st.info(summary_integration)
+
+col1, col2 = st.columns([4,3])
+
+with col1:
+    summary_integration = """
+    Integration is the opposite of differentiation.
+    \nFor a given function f(x), integration is used to find the area under the curve of the function between two points on the x-axis.
+    """
+    st.info(summary_integration)
+
+with col2:
+    st.video("https://www.youtube.com/watch?v=ni1DFNQnFJw&list=PLHnDkwDE03A88Tj3mrg_LRpUWqyVipHkM&index=101")
 
 
 st.markdown("#### Indefinite Integration")
@@ -24,19 +31,175 @@ st.write("""Definite integration is used to obtain a numerical answer for the ar
 col1, col2 = st.columns([4,3])
 
 with col1:
+    st.write("Example of a definite integral: Integrate (6x + 1) from 2 to 5.")
     latex_code = """
     \int_{2}^{5} (6x + 1) \,dx
     """
     st.latex(latex_code)
 
-    st.write("Integrate to obtain the integral:")
+    st.write("Integrate to obtain the integral.  The limits are displayednext to the square brackets.")
 
     latex_code = """
-    [3x^2 + x]{2}^{5}
+    = [3x^2 + x]_2^5
     """
     st.latex(latex_code)
+
+    latex_code = r"""
+    = (3 \times 5^2 + 5) - (3 \times 2^2 + 2)
+    = 80 - 14
+    = 66
+    """
+    st.latex(latex_code)
+
+    st.write("")
 
 with col2:
     st.video("https://www.youtube.com/watch?v=Sp_KWTdBRsE&list=PLHnDkwDE03A88Tj3mrg_LRpUWqyVipHkM&index=101&pp=iAQB")
 
 
+st.markdown("#### Integration Example with SymPy")
+
+st.write("To calculate the definite integral:")
+
+latex_code = """
+\int_{2}^{5} (6x + 1) \,dx
+"""
+st.latex(latex_code)
+
+st.write("The SymPy library in Python can be used...")
+
+code_snippet = """
+from sympy import symbols, integrate
+
+x = symbols("x")
+
+f = 6*x + 1
+
+# Calculate the definite integral from 2 to 5
+st.write(integrate(f, (x, 2, 5)))
+"""
+
+st.code(code_snippet)
+exec(code_snippet)
+
+
+st.markdown("#### Parametric Integrals")
+
+col1, col2 = st.columns([4,3])
+
+with col1:
+    pass
+
+with col2:
+    st.video("https://www.youtube.com/watch?v=p6YBlqon8TE&list=PLHnDkwDE03A88Tj3mrg_LRpUWqyVipHkM&index=102&pp=iAQB")
+
+
+st.markdown("#### Integration with Exponentials and Logarithms")
+
+col1, col2 = st.columns([4,3])
+
+with col1:
+    pass
+
+with col2:
+    st.video("")
+
+
+st.markdown("#### Integrating Trigonometric Functions")
+
+col1, col2 = st.columns([4,3])
+
+with col1:
+    pass
+
+with col2:
+    st.video("")
+
+
+st.markdown("#### Reverse Chain Rule")
+
+col1, col2 = st.columns([4,3])
+
+with col1:
+    pass
+
+with col2:
+    st.video("")
+
+
+st.markdown("#### Integration by Substitution")
+
+col1, col2 = st.columns([4,3])
+
+with col1:
+    pass
+
+with col2:
+    st.video("")
+
+
+st.markdown("#### Numerical Integration")
+
+col1, col2 = st.columns([4,3])
+
+with col1:
+    pass
+
+with col2:
+    st.video("")
+
+
+st.markdown("#### ")
+
+col1, col2 = st.columns([4,3])
+
+with col1:
+    pass
+
+with col2:
+    st.video("")
+
+
+st.markdown("#### ")
+
+col1, col2 = st.columns([4,3])
+
+with col1:
+    pass
+
+with col2:
+    st.video("")
+
+
+st.markdown("#### ")
+
+col1, col2 = st.columns([4,3])
+
+with col1:
+    pass
+
+with col2:
+    st.video("")
+
+
+st.markdown("#### ")
+
+col1, col2 = st.columns([4,3])
+
+with col1:
+    pass
+
+with col2:
+    st.video("")
+    
+
+
+st.markdown("#### ")
+
+col1, col2 = st.columns([4,3])
+
+with col1:
+    pass
+
+with col2:
+    st.video("")
