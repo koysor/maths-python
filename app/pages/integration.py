73cce67ca84c4f894229a25598d710aa7a0cc1af
Koysor <koysor@gmail.com>
2025-05-03 11:11:47 +0100

    Added integration file

--- app/pages/integration.py
+++ app/pages/integration.py
@@ -21,12 +21,46 @@ with col2:
 st.markdown("#### Indefinite Integration")
 
 st.write("""Indefinite integration, the opposite of differentiation, 
-is used to determine the original function which was differentiated in order to obtain the current function.""")
+is used to determine the **original function** which was differentiated in order to obtain the current function.""")
+
+st.write("Rule to integrate terms which are written in the form of $$ax^n$$:")
+
+latex_code = r"""
+\int ax^n \,dx = \frac{a}{n+1}x^{n+1} + C
+"""
+st.latex(latex_code)
+st.write("$$\int$$ is the symbol for integration.")
+st.write("We increase the power of x by 1 and then divide by the new power.")
+st.write("Add C which is the constant of integration.  When we differentiate, any constant terms will disapper.  Therefore many functions have the same derivative.")
+st.write("When we integrate, we need to add a constant term to the function to account for this.")
+
+st.write("Working example of indefinite integration:")
+
+latex_code = r"""
+\int 6x + 1 \,dx \\
+"""
+st.latex(latex_code)
+
+st.write("We can split the integral into two parts:")
+st.latex(r"""
+\int 6x \,dx = \frac{6}{1+1}x^{1+1} \\
+= 3x^2       
+""")
+
+st.latex(r"""
+\int 1 \,dx = x \\
+""")
+
+latex_code = r"""
+\frac{6}{1+1}x^{1+1} + 1 + C \\
+= 3x^2 + x + C  
+"""
+st.latex(latex_code)
 
 
 st.markdown("#### Definite Integration")
 
-st.write("""Definite integration is used to obtain a numerical answer for the area under the curve of a function between two points on the x-axis.""")
+st.write("""Definite integration is used to obtain a **numerical answer** for the area under the curve of a function between two points on the x-axis.""")
 
 col1, col2 = st.columns([4,3])
 
@@ -37,7 +71,7 @@ with col1:
     """
     st.latex(latex_code)
 
-    st.write("Integrate to obtain the integral.  The limits are displayednext to the square brackets.")
+    st.write("Integrate to obtain the integral.  The limits are displayed next to the square brackets.")
 
     latex_code = """
     = [3x^2 + x]_2^5
@@ -88,7 +122,12 @@ st.markdown("#### Parametric Integrals")
 col1, col2 = st.columns([4,3])
 
 with col1:
-    pass
+    st.write("This version of the **Chain Rule** can be used to find the gradient of a curve which is defined by **parametric equations**.")
+
+    latex_code = r"""
+    \frac{dy}{dx} = \frac{dy}{dt} \div \frac{dx}{dt}
+    """
+    st.latex(latex_code)
 
 with col2:
     st.video("https://www.youtube.com/watch?v=p6YBlqon8TE&list=PLHnDkwDE03A88Tj3mrg_LRpUWqyVipHkM&index=102&pp=iAQB")
