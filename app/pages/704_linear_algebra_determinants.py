ce22e9bed3392c8646abea8c3bcb17bb6e32bfc0
Koysor <koysor@gmail.com>
2025-05-11 07:18:00 +0100

    Added content to linear algebra determinants page

---
+++ app/pages/704_linear_algebra_determinants.py
@@ -0,0 +1,62 @@
+import streamlit as st
+
+import utils
+
+
+st.set_page_config(layout="wide")
+st.markdown("### Linear Algebra Determinants")
+
+
+st.write(
+    "The determinant is a **scalar value** that can be computed from the elements of a **square matrix**. It provides important properties of the matrix, such as whether it is **invertible**."
+)
+
+st.write("The determinant of a 2x2 matrix is calculated as follows:")
+st.latex(
+    r"M = "
+    + r"""
+\begin{bmatrix}
+a & b \\
+c & d
+\end{bmatrix} \\
+~ \\
+\text{det M} = ad - bc
+"""
+)
+
+st.info(
+    """
+Where **$$det~M = 0$$**, $$M$$ is a **singular matrix** and is **not** invertible. \r\n
+Where **$$det~M \\neq 0$$**, $$M$$ is a **non-singular matrix** and is invertible.
+"""
+)
+
+
+st.markdown("##### Example:")
+
+utils.display_run_python_snippet(
+    code_snippet="""
+from sympy import Matrix, latex                   
+
+A = Matrix( [ [6, 5], [1, 2] ] )
+st.latex(r'A = ' + latex(A))
+
+det_A = A.det()
+                                 
+st.latex(r'det~A = ' + latex(det_A))                                                
+"""
+)
+
+latex_code = r"""
+det~A = 6 * 2 - 5 * 1 = 12 - 5 = 7
+"""
+st.latex(latex_code)
+
+st.info(
+    """In general, for an $$n x m$$ matrix, the determinant tells you how the matrix scales n-dimensional volume."""
+)
+
+st.info(
+    """A positive determinant indicates the transformation preserves orientation (no flipping). 
+    \r\nA negative determinant means it reverses orientation (includes a reflection).."""
+)
