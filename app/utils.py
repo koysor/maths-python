437d1e208f2045a89da5c1a3e7f9fed8c23aad0f
Koysor <koysor@gmail.com>
2025-05-11 06:39:28 +0100

    Added utils.py

---
+++ app/utils.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+
+def display_run_python_snippet(code_snippet: str):
+    """
+    Display and run a Python code snippet in Streamlit.  Aligns the code to a width of 4/6ths of the screen to the left.
+    """
+
+    col1, _ = st.columns([4,2])
+
+    with col1:
+        st.code(code_snippet, language='python')
+        exec(code_snippet)
