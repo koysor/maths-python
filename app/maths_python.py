fdba07ff7d3d9bac53e85cb057346588e3b316dd
Koysor <koysor@gmail.com>
2025-06-06 12:42:57 +0100

    Minor updates

---
+++ app/maths_python.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+st.set_page_config(layout="wide")
+st.markdown("### Snippets of Python and LaTex Code for Maths Operations")
+
+
+st.write(
+    "Source code for this application can be found at https://github.com/koysor/maths-python ."
+)
--- launch_streamlit_app.sh
+++ launch_streamlit_app.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+echo "Starting the Streamlit app for Maths Python..."
+
 VENV_DIR=".venv"
-APP_SCRIPT="app/main.py"
+APP_SCRIPT="app/maths_python.py"
 
 source "$VENV_DIR/bin/activate"
 
