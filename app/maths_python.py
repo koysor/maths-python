a3eb5181ba2d3ef63e0e9fc7f7b45580476761ab
Koysor <koysor@gmail.com>
2025-06-15 09:27:07 +0100

    maths python streamlit homepage updated with image

--- app/maths_python.py
+++ app/maths_python.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pathlib
+from pathlib import Path
 
 
 st.set_page_config(layout="wide")
@@ -8,3 +10,12 @@ st.markdown("### Snippets of Python and LaTex Code for Maths Operations")
 st.write(
     "Source code for this application can be found at https://github.com/koysor/maths-python ."
 )
+
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("app/assets/images/maths_python", caption="Maths Python App", width=450)
+
+with col2:
+    st.write(f"Current working directory: {pathlib.Path().cwd()}")
