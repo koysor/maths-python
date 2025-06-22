4df60ea3a347d9743bd0c65ca1254956ec03c19b
Koysor <koysor@gmail.com>
2025-06-22 16:11:53 +0100

    formatting changes on prob dist streamlit page

--- app/pages/802_probability_distributions.py
+++ app/pages/802_probability_distributions.py
@@ -5,20 +5,20 @@ st.set_page_config(layout="wide")
 st.markdown("### Probability Distributions")
 
 st.write(
-    "Probability distributions describe how the values of a random variable are distributed. They can be classified into two main categories: discrete and continuous distributions."
+    "Probability distributions describe how the values of a random variable are distributed. " \
+    "They can be classified into two main categories: discrete and continuous distributions."
 )
 
 
-st.markdown("##### Probability Distribution Function (PDF)")
+st.markdown("##### Probability Density Function (PDF)")
 st.write(
-    "The probability distribution function (PDF) describes the likelihood of a random variable taking on a specific value. For discrete random variables, the PDF is represented as a probability mass function (PMF), while for continuous random variables, it is represented as a probability density function (PDF)."
+    "A PDF denoted $$f(x)$$ can be used to generate the probability that outcomes of a continuous distribution " \
+    "lie within a particular range of outcomes."
 )
 
 
 st.markdown("##### Cumulative Distribution Functions (CDF)")
 st.write(
-    "In simplete terms, the CDF sums up the probabillities of all possible outcomes up to a certain point. It gives the probability that a random variable is less than or equal to a certain value."
-)
-st.write(
-    "The cumulative distribution function (CDF) describes the probability that a random variable takes on a value less than or equal to a specific value. It is defined as the integral of the PDF from negative infinity to the specific value."
+    "In simplete terms, the CDF sums up the probabillities of all possible outcomes up to a certain point. " \
+    "It gives the probability that a random variable is less than or equal to a certain value."
 )
